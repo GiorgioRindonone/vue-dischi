e08682638c558578435432af4ee73881b3d65062
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-04-08 18:29:14 +0200

    update_start

---
+++ script.js
@@ -0,0 +1,9 @@
+// Attraverso una chiamata ajax all’API di boolean
+// https://flynn.boolean.careers/exercises/api/array/music
+// avremo a disposizione una decina di dischi musicali.
+// Utilizzando vue, stampiamo a schermo una card per ogni album.
+// BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+// BONUS 2: Ordinare i dischi per anno di uscita.
+// colore body #1E2D3B
+// colore header/card #2E3A46
+// font Lato
